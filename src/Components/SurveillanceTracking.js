3b6693c2e8254fdb16f45f7cac352f84ca30c3ee
Jensen Tan <tzhjensen142@gmail.com>
2025-03-16 22:19:01 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/Components/SurveillanceFrequency.js 	new file:   src/Components/SurveillanceFrequency.json 	modified:   src/Components/SurveillanceTracking.js

--- src/Components/SurveillanceTracking.js
+++ src/Components/SurveillanceTracking.js
@@ -2,7 +2,8 @@ const SurveillanceTracking = () => {
   return (
     <div style={{ width: "100%", height: "500px" }}>
       <iframe 
-src="https://epicovcharts.bii.a-star.edu.sg/hcov19-variants-dashboard/"        width="100%" 
+src="https://gisaid.org/rsv-subtypes-dashboard/"        
+width="100%" 
         height="100%" 
         style={{ border: "none" }}
         title="External Page"
